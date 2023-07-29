c3799aabd16770d873042a078173ea71c9fbeffa
sanofer <mssanofer@gmail.com>
2023-07-29 19:20:51 +0530

    task

--- day20/script.js
+++ day20/script.js
@@ -1,14 +1,33 @@
-async function fetchData() {
-  const apiUrl = "https://restcountries.com/v3.1/all";
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    for (var i = 0; i < data.length; i++) {
-      if (data[i].name.common == "Peru") {
-        console.log(data[i].flag);
-      }
+
+function search(){
+
+  var city = document.getElementById('city').value
+  var country = document.getElementById('country').value
+  
+  
+  var request = new XMLHttpRequest()
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&APPID=a9cfdb29a716e26d71aebb273da499d6"
+  request.open('GET',url,true)
+  request.send()
+  
+  request.onload = function(){
+    if(request.readyState == 4 && request.status == 200){
+    var data = JSON.parse(this.response);
+    var temp = (data["main"]["temp"]-273.15).toFixed(1);
+    document.querySelector('#weather').innerHTML = data["name"]+","+data["sys"]["country"]+" Temperature "+temp+"&deg, wind "+data["wind"]["speed"] +" m/s, pressure " +data["main"]["pressure"]+" hpa";
+    console.log(city,country);	
     }
-  } catch (err) {
-    console.log("Error fetching Data: ", err);
+    else{
+      alert("Status: "+request.status+" Message: "+request.statusText);
+      console.log("Status: "+request.status+" Message: "+request.statusText);
+    }
   }
-}
+    
+  request.onerror = function(){
+    console.log("connection failed")
+    alert("Connection failed");
+  }
+    
+  
+  }
+  
