4ca940c4c32bb8690ea29582fe8f90738a2a9431
sanofer <mssanofer@gmail.com>
2023-08-18 13:27:40 +0530

    hi

---
+++ React-FrontEnd/day-1/leno-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Home'
+
+function App() {
+  
+
+  return (
+    <>
+     <h2><Home/></h2>
+    </>
+  )
+}
+
+export default App
---
+++ React-FrontEnd/day-1/leno-app/src/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+      <h3>helllllllllllo</h3>
+      </div>
+    )
+  }
+}
+
+export default Home
