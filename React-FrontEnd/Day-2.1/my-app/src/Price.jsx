2d2ce7c0950d1e1312c84f742631f95969298dd9
sanofer <mssanofer@gmail.com>
2023-08-21 21:28:36 +0530

    hi

--- React-FrontEnd/Day-2.1/my-app/src/Price.jsx
+++ React-FrontEnd/Day-2.1/my-app/src/Price.jsx
@@ -2,9 +2,23 @@ import React, { Component } from 'react'
 
 export class Price extends Component {
   render() {
-    return (
-      <div>Price</div>
-    )
+    return  React.createElement('div',{className:"columns"},React.createElement('ul',{className:"price"},
+    React.createElement('li',{className:"header"},"Basic"),React.createElement('li',{className:"grey"},"$ 9.99 / year"),
+    React.createElement('li',null,"10GB Storage"),React.createElement('li',null,"10 Emails"),
+    React.createElement('li',null,"10 Domains"),React.createElement('li',null,"1GB Bandwidth"),
+    React.createElement('li',{className:"grey"},React.createElement('a',{href:"#",className:"button"},"Sign Up"))),
+    React.createElement('ul',{className:"price"},
+    React.createElement('li',{className:"header"},"Pro"),React.createElement('li',{className:"grey"},"$ 29.99 / year"),
+    React.createElement('li',null,"25GB Storage"),React.createElement('li',null,"25 Emails"),
+    React.createElement('li',null,"25 Domains"),React.createElement('li',null,"2GB Bandwidth"),
+    React.createElement('li',{className:"grey"},React.createElement('a',{href:"#",className:"button"},"Sign Up"))),
+    React.createElement('ul',{className:"price"},
+    React.createElement('li',{className:"header"},"Premium"),React.createElement('li',{className:"grey"},"$ 49.99 / year"),
+    React.createElement('li',null,"50GB Storage"),React.createElement('li',null,"50 Emails"),
+    React.createElement('li',null,"50 Domains"),React.createElement('li',null,"5GB Bandwidth"),
+    React.createElement('li',{className:"grey"},React.createElement('a',{href:"#",className:"button"},"Sign Up"))),)
+    
+    
   }
 }
 
