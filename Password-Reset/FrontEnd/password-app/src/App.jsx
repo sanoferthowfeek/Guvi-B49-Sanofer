2ae1d32c3020e522d461aaa0046476869cc0587f
sanofer <mssanofer@gmail.com>
2023-09-25 12:04:10 +0530

    password reset

--- Password-Reset/FrontEnd/password-app/src/App.jsx
+++ Password-Reset/FrontEnd/password-app/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+import Error from "./components/Error";
+import PasswordReset from "./components/PasswordReset";
+import ForgotPassword from "./components/ForgotPassword";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import { Routes, Route, useNavigate } from "react-router-dom"
+import { useEffect, useContext, useState } from "react";
+import { LoginContext } from "./components/ContextProvider/Context";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState(false);
+
+  const { logindata, setLoginData } = useContext(LoginContext);
+
+
+  const history = useNavigate();
+
+  const DashboardValid = async () => {
+    let token = localStorage.getItem("usersdatatoken");
+
+    const res = await fetch("/validuser", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token
+      }
+    });
+
+    const data = await res.json();
+
+    if (data.status == 401 || !data) {
+      console.log("user not valid");
+    } else {
+      console.log("user verify");
+      setLoginData(data)
+      history("/dash");
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(()=>{
+      DashboardValid();
+      setData(true)
+    },2000)
+
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        data ? (
+          <>
+            <Header />
+
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/dash" element={<Dashboard />} />
+              <Route path="/password-reset" element={<PasswordReset />} />
+              <Route path="/forgotpassword/:id/:token" element={<ForgotPassword />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </>
+
+        ) : <Box sx={{ display: 'flex', justifyContent: "center", alignItems: "center", height: "100vh" }}>
+          Loading... &nbsp;
+          <CircularProgress />
+        </Box>
+      }
+
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
