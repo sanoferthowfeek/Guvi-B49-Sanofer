d4d667099af0ca1d429f54bacd2352746dce7f10
sanofer <mssanofer@gmail.com>
2023-11-04 18:57:19 +0530

    task

---
+++ task/callbackhell/script.js
@@ -0,0 +1,38 @@
+var b =document.getElementById('hell')
+let a  = () => {
+b.innerText = '10';
+   setTimeout(  ()=>{
+    b.innerText = '9'
+    setTimeout(  () =>{
+        b.innerText = '8'
+        setTimeout(  ()=>{
+            b.innerText = '7'
+            setTimeout(   ()=>{
+                b.innerText = '6'
+                setTimeout(()=>{
+                    b.innerText = '5'
+                    setTimeout(    
+                        ()=>{
+                            b.innerText = '4'
+                            setTimeout(() =>{
+                            b.innerText = '3'
+                            setTimeout( 
+                              () => {
+                               b.innerText = '2'
+                                 setTimeout(() => {
+                                   b.innerText = '1'
+                                 setTimeout( () => {
+                               b.innerText = 'Happy Independence Day'
+                           },1000)
+                         },1000)
+                       },1000)
+                    },1000)
+                },1000)
+            },1000)
+          },1000)
+        },1000)
+     },1000)
+  },1000);
+}
+
+ window.onload = a();
